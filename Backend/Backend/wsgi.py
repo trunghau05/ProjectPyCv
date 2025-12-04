df6d7270b7a79a9694bdaf3416fd299adf51fe1a
hao anh <167725453+leanhhao03@users.noreply.github.com>
2025-12-04 23:25:35 +0700

    Refactor WSGI configuration to include project path in Python path

--- Backend/Backend/wsgi.py
+++ Backend/Backend/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys  # <--- BƯỚC 1: IMPORT THƯ VIỆN SYS
 from django.core.wsgi import get_wsgi_application
+
+# <--- BƯỚC 2: THÊM CODE THÊM ĐƯỜNG DẪN DỰ ÁN VÀO PYTHON PATH
+path_to_backend = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+if path_to_backend not in sys.path:
+    sys.path.insert(0, path_to_backend)
+# ------------------------------------------------------------------------------------
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.Backend.settings')
 
